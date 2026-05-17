ed02ed142943f0b4c985ead224b9bd70fa8b7a7a
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2026-05-17 19:36:37 -0400

    Fixed compile errors

--- LtScience/KCT_Interface.cs
+++ LtScience/KCT_Interface.cs
@@ -3,9 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-//using KerbalConstructionTime;
+using KerbalConstructionTime;
 using UnityEngine;
 //using KSP_Log;
+
 
 namespace LtScience
 {
--- LtScience/Utilities/StockSettings.cs
+++ LtScience/Utilities/StockSettings.cs
@@ -2,7 +2,7 @@ using KSP.Localization;
 using System.Collections;
 using System.Reflection;
 
-namespace LtScience.Utilities
+namespace LtScience
 {
     // http://forum.kerbalspaceprogram.com/index.php?/topic/147576-modders-notes-for-ksp-12/#comment-2754813
     // search for "Mod integration into Stock Settings
